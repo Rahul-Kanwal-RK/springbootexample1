7a6428393913b7c6570a6876bc138aa7627b7e2c
Rahul Kanwal <rahulkanwal1999@gmail.com>
2024-09-27 21:56:11 -0700

    CORS Fix

--- src/main/java/com/example/firstAPI/api/controller/UserController.java
+++ src/main/java/com/example/firstAPI/api/controller/UserController.java
@@ -3,11 +3,15 @@ package com.example.firstAPI.api.controller;
 import com.example.firstAPI.api.model.User;
 import com.example.firstAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@CrossOrigin
+@RequestMapping("*")
 @RestController
 public class UserController {
     private UserService userService;
